fa594471712e56fd741f5b2a604235840fb0979a
Polynomeer <polynomeer@gmail.com>
2021-03-09 11:20:23 +0900

    Initialize git repository

---
+++ src/main/java/net/slipp/AugustSlippApplication.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustSlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugustSlippApplication.class, args);
+	}
+
+}
